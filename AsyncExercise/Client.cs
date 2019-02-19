9345219a3f2a8bf9a732fbdcf2fa4a67cff15e83
Ole Eriksen <oe@easv.dk>
2019-02-19 20:08:53 +0100

    init

---
+++ AsyncExercise/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace AsyncExercise
+{
+    public class Client
+    {
+        private readonly Server mServer;
+
+        public Client(Server server)
+        {
+            mServer = server;
+        }
+
+        public void run()
+        {
+            Console.WriteLine("Here are the numbers from the server: ");
+
+            int[] numbers = mServer.GetNumbers(20, 1, 5);
+
+            foreach (int x in numbers)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ AsyncExercise/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace AsyncExercise
+{
+    public class Server
+    {
+
+        public int[] GetNumbers(int amount, int min, int max)
+        {
+            int[] res = new int[amount];
+            Random r = new Random();
+            int count = 0;
+            while (count < amount)
+            {
+                int x = r.Next(max - min + 1) + min;
+                res[count++] = x;
+            }
+            return res;
+        }
+    }
+}
